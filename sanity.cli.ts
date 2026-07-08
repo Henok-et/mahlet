7bef63b1a64c39bf5894cb7439cae33a342b5f76
Henok-et <henok.kinde-ug@aau.edu.et>
2026-07-08 11:44:27 +0300

    feat: update Sanity project ID and initialize reusable API client

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,6 @@
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "lynda-gratton-cms";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "8c7h6llw";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineCliConfig({
